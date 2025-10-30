782eb27f7cb4301ef00ed332a80a23bccbe561b1
Sathyajith94 <sathyajith.sfhl@gmail.com>
2025-10-30 17:13:33 +0530

    signin wth pwd

--- auth.js
+++ auth.js
@@ -30,15 +30,43 @@ import { supabase } from './supabase.js';
 // );
 
 // Signup with redirect url
-const { data, error } = await supabase.auth.signUp(
-    {
-        email: "sathyajith.sfhl@gmail.com",
-        password: "Sidharth001",
-        options: {
-            emailRedirectTo: 'https://sidharth-housing.web.app/'
-        }
-    }
-);
+// const { data, error } = await supabase.auth.signUp(
+//     {
+//         email: "sathyajith.sfhl@gmail.com",
+//         password: "Sidharth001",
+//         options: {
+//             emailRedirectTo: 'https://sidharth-housing.web.app/'
+//         }
+//     }
+// );
+
+// create an anonymously user
+// const { data, error } = await supabase.auth.signInAnonymously({
+//     options: {
+//         captchaToken: "eNv6Yu"
+//     }
+// });
+
+// create an anonymously user with custom user metadata
+// const { data, error } = await supabase.auth.signInAnonymously({
+//     options: {
+//         data: {
+//             name: "sathya"
+//         }
+//     }
+// });
+
+// SIgnin with passward
+// const { data, error } = await supabase.auth.signInWithPassword({
+//     email: "sathyajith.sfhl@gmail.com",
+//     password: "Sidharth001"
+// });
+
+// SIgnin with passward
+const { data, error } = await supabase.auth.signInWithPassword({
+    phone: '9790787189',
+    password: 'Sidharth001',
+})
 
 if (error) console.error("Error:", error);
 console.log('data: ', data);
